659caafcafcb46b9dde4f1458ab6571d24a71f55
Bhavishya9 <Bhavishya9@missionrnd.com>
2016-01-19 04:56:58 +0530

    Submit Commit

--- src/Consonants_Vowels.cpp
+++ src/Consonants_Vowels.cpp
@@ -21,6 +21,24 @@ NOTES: Don't create new string , Dont return anything ,you have been given two p
 
 
 void count_vowels_and_consonants(char *str,int *consonants, int *vowels){
-
-
+	if (str == NULL)//check if the string is null
+	{
+		*consonants = 0;//if it is then make the pointer variables point to zero 
+		*vowels = 0;
+	}
+	else
+	{
+		int no_of_consonants = 0, no_of_vowels = 0;
+		for (int index = 0; str[index] != '\0'; index++)//scan from the start of the string to end
+		{
+			if (str[index] >= 'A' && str[index] <= 'Z')//convert all the uppercase letters to lower case letters
+				str[index] = str[index] + 32;
+			if (str[index] == 'a' || str[index] == 'e' || str[index] == 'i' || str[index] == 'o' || str[index] == 'u')//check for vowels
+				no_of_vowels++; //if it is found increment the no of vowels 
+			else if (str[index] >= 'a' && str[index] <= 'z')//if it is in the range of lowercase alphabets
+				no_of_consonants++;//then increment the no of consonants
+		}
+		*consonants = no_of_consonants;//consonants point to the no of consonants
+		*vowels = no_of_vowels;//vowels point to the no of vowels
+	}
 }
--- src/getLastWord.cpp
+++ src/getLastWord.cpp
@@ -11,6 +11,34 @@ Note:Dont modify original string Neglect Spaces at the right end and at left end
 #include <stdlib.h>
 
 char * get_last_word(char * str){
-
-	return NULL;
+	if (str == NULL)	//check if the string is null 
+		return NULL;	//if it is null then return null
+	int size = 0, index = 0, length = 0;
+	while (str[index++] != '\0')	//calculate the length of the given string
+		length++;
+	index = index - 2;	//place the index at last letter in the string
+	while (str[index] == ' ' && index > 0)	//skip all the spaces at the end
+		index--;	//continue until we hit an alphabet or we reach the end of the string
+	if (index <= 0 && length == 0)	//if we reached the end of the string and 
+		return "";	//there arent any alphabets then return ""
+	int index1 = 0;
+	char *temp = (char*)malloc(size*sizeof(char));	//create a temp array
+	for (int i = index; str[i] != ' ' && i >= 0; i--) //copy the last word into temp array from back
+	{
+		size++;
+		temp = (char *)realloc(temp, size);
+		temp[index1] = str[i];
+		index1++;
+	}
+	temp[index1] = '\0';
+	char *s = (char *)malloc(size*sizeof(char)); // create the resultant array 
+	length = size;	//re assign the value of length with size of the temp array
+	int index2 = 0;
+	for (index2 = 0; index2 < size; index2++) //reverse the temp 
+	{
+		s[index2] = temp[length - 1]; //the reversed string is stored in s array
+		length--;
+	}
+	s[index2] = '\0';
+	return s; //return s array which is the resultant
 }
