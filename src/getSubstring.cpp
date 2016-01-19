ceae937388944146bb1522c13d352ac2d0f8c5ce
Bhavishya9 <Bhavishya9@missionrnd.com>
2016-01-19 05:39:03 +0530

    Submit Commit

--- src/getSubstring.cpp
+++ src/getSubstring.cpp
@@ -18,6 +18,20 @@ original String
 #include <stdlib.h>
 
 char * get_sub_string(char *str, int i, int j){
-
-    return NULL;
+	if (str == NULL	||	i > j	)	//return NULL when either str is NULL or the value of i is 
+		return NULL;				//greater than j because they are invalid inputs.
+	int index = 0,length=0;
+	while (str[index++] != '\0')	//calculate the length of the string
+		length++;
+	if (i > length-1 || j > length-1)//if either i or j are greater than the value of the index
+		return NULL;//of the last char in the string then return NULL as they i,j values are out of bounds for the string
+	int size = j - i + 1;//if all the values are valid then create a result with size j-1-i
+	char *result = (char *)malloc(size*sizeof(char));//allocate the memory dynamically
+	int index1 = 0;
+	for (index = i; index <= j; index++)//start from ith index in input string and continue to 
+	{									//copy the values till jth index in input string
+		result[index1++] = str[index];	
+	}
+	result[index1] = '\0';//assign null value at the end of the string
+	return result; //return resultant string 
 }
