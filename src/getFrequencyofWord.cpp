c4451b601e6d91613bac50cdbe54bf486e61c15b
Bhavishya9 <Bhavishya9@missionrnd.com>
2016-01-24 15:26:12 +0530

    Submit Commit

--- src/getFrequencyofWord.cpp
+++ src/getFrequencyofWord.cpp
@@ -13,7 +13,64 @@ Note: Dont modify original str or word,Just return count ,Spaces can also be par
 #include <stdlib.h>
 
 int count_word_in_str_way_1(char *str, char *word){
-	return 0;
+	if (str == NULL || word == NULL)
+		return NULL;
+	int length1 = 0, length2 = 0, index = 0, index1 = 0, index2 = 0, w = 0, flag = 0,count=0;
+	char *s = (char *)malloc(length1*sizeof(char));
+	char *su = (char *)malloc(length2*sizeof(char));
+	while (str[index1] != '\0')
+	{
+		if (str[index1] == ' ')
+		{
+			index1++;
+			continue;
+		}
+		length1++;
+		s = (char *)realloc(s, length1);
+		s[index++] = str[index1++];
+	}
+	s[index] = '\0';
+	index = 0;
+	while (word[index2] != '\0')
+	{
+		if (word[index2] == ' ')
+		{
+			index2++;
+			continue;
+		}
+		length2++;
+		su = (char *)realloc(su, length2);
+		su[index++] = word[index2++];
+	}
+	su[index] = '\0';
+	if (s[0] == NULL && su[0] == NULL)
+	{
+		flag = 1;
+		w = index1 / index2;
+	}
+	if (flag == 0){
+		for (index1 = 0; index1 < length1; index1++)
+		{
+			count = 0;
+			int k = index1;
+			for (index2 = 0; su[index2] != '\0'; index2++)
+			{
+				if (s[k] != su[index2])
+				{
+					k++;
+					break;
+				}
+				else
+				{
+					count = count + 1;
+					k++;
+				}
+			}
+			if (count == length2)
+				w = w + 1;
+		}
+	}
+	return w;
 }
 
 int count_word_int_str_way_2_recursion(char *str, char *word){
